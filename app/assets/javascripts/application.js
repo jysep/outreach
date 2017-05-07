15493fb74bfa00224d7cb4a1cb58401e61853c9b
Eric Rykwalder <e.rykwalder@gmail.com>
2017-05-07 15:15:00 -0700

    remove rails ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require turbolinks
 //= require_tree .
