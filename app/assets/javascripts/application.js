a8fd5815440cd4417aa399722e9d5cac93fd18a7
Eric Rykwalder <e.rykwalder@gmail.com>
2017-05-07 15:08:35 -0700

    change assets to cdn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require rails-ujs
 //= require turbolinks
-//= require bootstrap
 //= require_tree .
