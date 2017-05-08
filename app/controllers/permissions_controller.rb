e59ba2c315bb175c972650721ee2faa119d3335d
Eric Rykwalder <e.rykwalder@gmail.com>
2017-05-07 22:24:02 -0700

    add level to create permission

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -3,7 +3,7 @@ class PermissionsController < ApplicationController
 
 	def create
 		if Permission.where(email: current_user.email, campaign_id: params[:campaign_id], level: "owner").exists?
-			Permission.create!(email: params[:email], campaign_id: params[:campaign_id])
+			Permission.create!(email: params[:email], campaign_id: params[:campaign_id], level: "user")
 		end
 		redirect_to campaign_path(params[:campaign_id])
 	end
