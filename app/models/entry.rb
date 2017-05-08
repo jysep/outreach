12c037de333e151aee5840abc2a1639650d886c8
Eric Rykwalder <e.rykwalder@gmail.com>
2017-05-07 18:04:22 -0700

    keep kv mapping of options

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,27 +1,51 @@
 class Entry < ApplicationRecord
-	OUTCOMES = ["not home", "not visited", "no answer", "not interested", "interested"]
-	AGE_GROUPS = ["1", "2", "3"]
-	THEMES = (1..13).map {|i| "#{i}"}
+	OUTCOMES = {
+		"1" => "Not Home",
+		"2" => "Not Visited",
+		"3" => "No Answer",
+		"4" => "Not Interested",
+		"5" => "Interested"
+	}
+	AGE_GROUPS = {
+		"1" => "Children",
+		"2" => "Junior Youth",
+		"3" => "Youth"
+	}
+	THEMES = {
+			"1" => "Walking a Path of Service",
+			"2" => "Period of Youth",
+			"3" => "Twofold Moral Purpose",
+			"4" => "Constructive and Destructive Forces",
+			"5" => "Material and Spiritual Progress",
+			"6" => "Individual and Collective Transformation",
+			"7" => "A United Community",
+			"8" => "Community Building",
+			"9" => "Service to the Community",
+			"10" => "Early Adolescence",
+			"11" => "Educating Younger Generations",
+			"12" => "Institute Process",
+			"13" => "The Baha'i Faith"
+	}
 
 	belongs_to :campaign
 	validates :team, presence: true
 	validates :date, presence: true
 	validates :street, presence: true
 	validates :street_number, presence: true
-	validates :outcome, presence: true, inclusion: { in: OUTCOMES }
+	validates :outcome, presence: true, inclusion: { in: OUTCOMES.keys }
 	validate :validate_age_groups
 	validate :validate_themes
 
 	before_save :uniq_arrays
 
 	def validate_age_groups
-		if !age_groups.is_a?(Array) || age_groups.detect{|a| !AGE_GROUPS.include?(a)}
+		if !age_groups.is_a?(Array) || age_groups.detect{|a| !AGE_GROUPS.keys.include?(a)}
 			errors.add(:age_groups, :invalid)
 		end
 	end
 
 	def validate_themes
-		if !themes.is_a?(Array) || themes.detect{|t| !THEMES.include?(t)}
+		if !themes.is_a?(Array) || themes.detect{|t| !THEMES.keys.include?(t)}
 			errors.add(:themes, :invalid)
 		end
 	end
